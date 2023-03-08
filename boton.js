8c82bf03a84c3eae532fc0e81bde9c0f48a262bb
EntregaCv <127266288+EntregaCv@users.noreply.github.com>
2023-03-07 23:07:20 -0300

    Add files via upload

---
+++ boton.js
@@ -0,0 +1,8 @@
+function mostrarContenido() {
+	var contenido = document.getElementById("contenido");
+	if (contenido.style.display === "none") {
+		contenido.style.display = "block";
+	} else {
+		contenido.style.display = "none";
+	}
+}
